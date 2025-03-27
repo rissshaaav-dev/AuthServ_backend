b1df40b749144bd6e2ddf068044a7d438d7942be
rissshaaav-dev <191943910+rissshaaav-dev@users.noreply.github.com>
2025-03-27 18:44:20 +0530

    feat: enhance B2C signup route with input validation and project middleware

--- routes/b2c.route.js
+++ routes/b2c.route.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import b2cSignup from "../controllers/b2c.controllers/b2cSignup.controller.js";
+import validateProjectMiddleware from "../middlewares/validateProject.middleware.js";
+import validateB2cInput from "../middlewares/validateB2cInput.middleware.js";
 
-const b2cRouter = Router();
+const b2cRouter = Router({ mergeParams: true });
 
-b2cRouter.post("/signup", b2cSignup);
+b2cRouter.post(
+    "/signup",
+    validateProjectMiddleware, // if success, gets project from database and sends projectId and settings to the next middleware
+    validateB2cInput, // based on the settings provided by the previous middleware, validates user inputs
+    b2cSignup
+);
 
 export default b2cRouter;
